bd6ab297ee88766d8142203a0a9dbc7a5a9e4448
Rony Nogueira <ronynogueiras@gmail.com>
2016-11-25 00:23:58 -0200

    adicionada tabs para navegação

--- android/AdoPet/app/src/main/java/br/ufg/inf/adopet/activities/MainActivity.java
+++ android/AdoPet/app/src/main/java/br/ufg/inf/adopet/activities/MainActivity.java
@@ -1,15 +1,65 @@
 package br.ufg.inf.adopet.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import br.ufg.inf.adopet.R;
+import br.ufg.inf.adopet.adapters.PagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        this.setUpTabs();
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+
+
+        PagerAdapter pager = new PagerAdapter(getSupportFragmentManager(),mTabLayout.getTabCount(),this);
+        mViewPager.setAdapter(pager);
+
+        mTabLayout.addOnTabSelectedListener(this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+    private void setUpTabs(){
+
+        String[] tabs = getResources().getStringArray(R.array.tabs);
+
+        for (String tab : tabs){
+            mTabLayout.addTab(mTabLayout.newTab().setText(tab));
+        }
+
     }
 }
---
+++ android/AdoPet/app/src/main/java/br/ufg/inf/adopet/adapters/PagerAdapter.java
@@ -0,0 +1,49 @@
+package br.ufg.inf.adopet.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import br.ufg.inf.adopet.fragments.AccountFragment;
+import br.ufg.inf.adopet.fragments.AffiliationsFragment;
+import br.ufg.inf.adopet.fragments.DefaultAccountFragment;
+import br.ufg.inf.adopet.fragments.PostFeedFragment;
+
+/**
+ * Created by rony on 24/11/16.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    private int numberOfTabs;
+    private boolean auth;
+
+    public PagerAdapter(FragmentManager fm, int numberOfTabs, boolean auth) {
+        super(fm);
+        this.numberOfTabs = numberOfTabs;
+        this.auth = auth;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                return new PostFeedFragment();
+            case 1:
+                return new AffiliationsFragment();
+            case 2:
+                //Check for user login
+                return auth ? new AccountFragment() : new DefaultAccountFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfTabs;
+    }
+}
