4d34fa186ebec167eb8d418bb26e1be13059ed1d
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-12-14 10:08:43 -0200

    registration controller

---
+++ api/app/controllers/registrations_controller.rb
@@ -0,0 +1,5 @@
+class RegistrationsController < ::DeviseTokenAuth::RegistrationsController
+    def sign_up_params
+        params.permit(:name, :email, :password, :password_confirmation)
+    end
+end
