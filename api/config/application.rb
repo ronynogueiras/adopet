fe45a1077820302b133ddcb94de450458e059aac
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-12-14 10:07:25 -0200

    setup for devise token auth

--- api/config/application.rb
+++ api/config/application.rb
@@ -16,7 +16,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Api
+module AdopetApi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -26,5 +26,6 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.default_locale = :'pt-BR'
   end
 end
